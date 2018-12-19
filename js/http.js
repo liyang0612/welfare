6264704de61281ec67d4f725eafa6ca859f2bfcb
李洋 <liyang@rsung.com>
2018-12-19 23:48:58 +0800

    更新页面

---
+++ js/http.js
@@ -0,0 +1,28 @@
+function $http(params) {
+	mui.ajax('http://211.149.179.254:7001' + params.url, {
+		data: params.data || {},
+		dataType: 'JSON',
+		type: params.type || 'post',
+		timeout: 10000,
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		success: function(resStr) {
+			try {
+				var res = JSON.parse(resStr);
+				if(res.code == 10000) {
+					params.success(res)
+				}else {
+					plus.nativeUI.toast('异常状态：' + res.code);
+				}
+			}
+			catch(err) {
+				throw new Error(err);
+				plus.nativeUI.toast('500 error 数据返回异常');
+			}
+		},
+		error: params.error || function(error) {
+			plus.nativeUI.toast('http请求异常');
+		}
+	})
+}
