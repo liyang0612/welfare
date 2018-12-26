9a710c82b39cdd96160578bfa74bcbdd2d7f7bf1
李洋 <liyang@rsung.com>
2018-12-26 21:31:48 +0800

    接口对接更新

--- js/config.js
+++ js/config.js
@@ -10,11 +10,8 @@ function $http(params) {
 		success: function(resStr) {
 			try {
 				var res = JSON.parse(resStr);
-				if(res.code == 10000) {
-					params.success(res)
-				}else {
-					plus.nativeUI.toast('异常状态：' + res.code);
-				}
+				params.success(res)
+				
 			}
 			catch(err) {
 				throw new Error(err);
@@ -25,4 +22,22 @@ function $http(params) {
 			plus.nativeUI.toast('http请求异常');
 		}
 	})
+}
+
+function cardConfig(name) {
+	var cardObj = {
+		"工商银行": 'icbc',
+		"中国农业银行": 'abc',
+		"中国银行": 'boc',
+		"中国建设银行": 'ccb',
+		"中国邮政储蓄银行": 'psbc',
+		"招商银行": 'cmb',
+		"交通银行": 'bocom',
+		"成都银行": 'bocd',
+		"兴业银行": 'cib',
+		"浦发银行": 'spdb',
+		"中信银行": 'citic'
+	};
+	if(!cardObj[name]) return 'unionpay';
+	return cardObj[name];
 }
